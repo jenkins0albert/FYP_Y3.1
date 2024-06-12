7d928503df5f62f08e63f68517c467c98b8e20a9
Adriiiiiiiian <157805676+jenkins0albert@users.noreply.github.com>
2024-06-13 01:07:20 +0800

    created game manager

--- Assets/Adrian/Scripts/Game Manager/GameManager.cs
+++ Assets/Adrian/Scripts/Game Manager/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,5 +11,50 @@ public class GameManager : MonoBehaviour
 
     private PlayerInteraction activeplayer;
 
-    
+    public static GameManager Instance;
+    private void Awake()
+    {
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+
+        }
+
+        else 
+        {
+            DontDestroyOnLoad(gameObject);
+            
+            SceneManager.activeSceneChanged += SpawnPlayerOnSceneLoad;
+
+            Instance = this;
+        }
+        
+    }
+
+    private void SpawnPlayerOnSceneLoad(Scene currentscene, Scene nextscene)
+    {
+        if (activeplayer == null)
+        {
+            SpawnPoint spawnpoint = FindObjectOfType<SpawnPoint>();
+            GameObject newplayer = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+            Cursor.lockState = CursorLockMode.Locked;
+            activeplayer = newplayer.GetComponent<PlayerInteraction>();
+
+            activeplayer.transform.position = spawnpoint.transform.position;
+            activeplayer.transform.rotation = spawnpoint.transform.rotation;
+        }
+
+        else 
+        {
+            activeplayer = FindObjectOfType<PlayerInteraction>();
+            SpawnPoint spawnpoint = FindObjectOfType<SpawnPoint>();
+            activeplayer.transform.position = spawnpoint.transform.position;
+            activeplayer.transform.rotation = spawnpoint.transform.rotation;
+        }
+
+
+    }
+
+
 }
