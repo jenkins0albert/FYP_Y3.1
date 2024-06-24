84023f11b1678c0f1dbb4dbda09d3dbf6df0190f
indomiebrand <krrrmina@hotmail.com>
2024-06-25 02:33:05 +0800

    chasing playtest area

---
+++ Assets/Script/Chaser.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Chaser : MonoBehaviour
+{
+    
+    NavMeshAgent agentComponent;
+
+    [SerializeField]
+    Transform thingToChase;
+
+    private void Awake()
+    {
+        agentComponent = GetComponent<NavMeshAgent>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (thingToChase != null) { 
+        agentComponent.SetDestination(thingToChase.position);
+        }
+    }
+}
