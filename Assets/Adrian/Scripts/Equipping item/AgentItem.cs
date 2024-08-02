29aa93287e444f21cbe857b8d7167cd56fae7af6
Adriiiiiiiian <157805676+jenkins0albert@users.noreply.github.com>
2024-08-02 16:58:31 +0800

    inventory script works like 100% now

--- Assets/Adrian/Scripts/Equipping item/AgentItem.cs
+++ Assets/Adrian/Scripts/Equipping item/AgentItem.cs
@@ -5,6 +5,7 @@ using Inventory.Model;
 using System;
 using System.Diagnostics.Contracts;
 using System.Data.Common;
+using TMPro;
 
 public class AgentItem : MonoBehaviour
 {
@@ -16,54 +17,89 @@ public class AgentItem : MonoBehaviour
 
     [SerializeField]
     private List<ItemParameter> parametersToModify, itemCurrentState;
-
+    public TextMeshProUGUI unequipText;
 
     public Transform itemSpawn;
 
     public void SetItem(EquippableItemSO itemItemSO, List<ItemParameter> itemState)
     {
+
+        
+        DestroyAllChildrenImmediate(itemSpawn);
         if (item != null)
         {
             inventoryData.AddItem(item, 1, itemCurrentState);
                
         }
+
+
+        /////////////////////////////
+
+
+        unequipText.text = "Unequip [F]";
         this.item = itemItemSO;
+        this.item.DefaultParametersList = itemItemSO.DefaultParametersList; 
+        Debug.Log("current");
+      
         this.itemCurrentState = new List<ItemParameter>(itemState);
-        
+
+        GameObject itemEquip = this.itemCurrentState[0].equipObject;
+
+        Debug.Log("Item is called = " + itemCurrentState[0].equipObject.name);
+
+        GameObject instantiatedObject = Instantiate(itemEquip, Vector3.zero, Quaternion.identity);
+        instantiatedObject.transform.rotation = itemEquip.transform.rotation;
+        instantiatedObject.transform.SetParent(itemSpawn.transform, false);
+        //////////////////////////////
+
+
+        Debug.Log(itemSpawn.transform.position);
+        //float newValue = itemCurrentState[index].value + parameter.value;
+
         ModifyParameter();
     }
-
-   
-
-    
-
+    public void UnequipItem()
+    {
+        if (item != null)
+        {
+            DestroyAllChildrenImmediate(itemSpawn);
+            unequipText.text = " ";
+            inventoryData.AddItem(item, 1, itemCurrentState);
+            item = null;
+        }
+    }
+    public void DestroyAllChildrenImmediate(Transform itemNest)
+    {
+        foreach (Transform child in itemNest)
+        {
+            DestroyImmediate(child.gameObject);
+        }
+    }
+    public void Start()
+    {
+        unequipText.text = " ";
+    }
     private void ModifyParameter()
     {
         
+
+
         foreach (var parameter in parametersToModify)
         {
             if (itemCurrentState.Contains(parameter))
             {
 
-                GameObject itemNest = GameObject.Find("NestItemInventory");
+               
                 int index = itemCurrentState.IndexOf(parameter);
-                GameObject itemEquip = parameter.equipObject;
-
                 
-
-                GameObject instantiatedObject = Instantiate(itemEquip, Vector3.zero, Quaternion.identity);
-
-                instantiatedObject.transform.SetParent(itemSpawn.transform, false);
-
                 
-
-                Debug.Log(itemSpawn.transform.position);
-                //float newValue = itemCurrentState[index].value + parameter.value;
                 itemCurrentState[index] = new ItemParameter
                 {
                     itemParameter = parameter.itemParameter,
-                    
+                    equipObject = parameter.equipObject,
                     //value = newValue,
+                    itemName = parameter.itemName,
+                    
 
                 };
             }
