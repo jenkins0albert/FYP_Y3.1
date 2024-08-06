59907053f5faf2c166741e366c93df6f69a146a1
indomiebrand <krrrmina@hotmail.com>
2024-08-07 00:47:59 +0800

    Added voicelines in starting scene

--- Assets/CameraShake/CharTrigger.cs
+++ Assets/CameraShake/CharTrigger.cs
@@ -8,19 +8,13 @@ public class CharTrigger : MonoBehaviour
     public AudioSource playSound;
     public GameObject globalVol;
 
-    void OnTriggerStay(Collider other)
-    {
-        StartCoroutine(cameraShake.Shake(.01f));
-    }
     void OnTriggerEnter()
     {
         playSound.Play();
         globalVol.SetActive(true);
+        StartCoroutine(cameraShake.Shake(.01f));
+
     }
-    void OnTriggerExit()
-    {
-        playSound.Stop();
-        globalVol.SetActive(false);
-    }
+
 }
 
